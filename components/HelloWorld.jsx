638b2c966c1e7d5ceb1762c71c6a358bd22e1435
moritzmhmk <moritzmhmk@gmail.com>
2016-11-13 00:37:56 +0100

    added file upload

--- components/HelloWorld.jsx
+++ components/HelloWorld.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
+import request from 'superagent'
 
 export default class HelloWorld extends Component {
   render () {
@@ -7,8 +9,32 @@ export default class HelloWorld extends Component {
       <div className='helloworld'>
           {this.props.greeting}
           <button onClick={() => setGreeting('Hello Universe')} >Change</button>
+          <br />
+          <Dropzone onDrop={this.onDrop}>
+            <div>Try dropping some files here, or click to select files to upload.</div>
+          </Dropzone>
       </div>
     )
+  }
+
+  onDrop (files) {
+    console.log('Received files: ', files)
+    files.forEach((file) => {
+      request
+      .post('/api/upload/' + file.name)
+      .send(file)
+      .on('progress', function (e) {
+        console.log('Percentage done: ', e.percent)
+      })
+      .end(function (err, res) {
+        console.log(err, res)
+        if (err) {
+          console.error(err)
+        } else {
+          console.log(JSON.stringify(res.body))
+        }
+      })
+    })
   }
 }
 
